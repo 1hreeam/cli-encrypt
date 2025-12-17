f41078f87d8a3c7eb8041eb1f292dc5ea15d9f72
1hreeam <hypexund@gmail.com>
2025-12-17 18:59:12 +0100

    add the masked password input via inquirer

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,5 @@
 import type { Command } from "commander";
+import inquirer from "inquirer";
 import path from "path";
 import { encryptFile, decryptFile } from "./crypto.js";
 
@@ -7,11 +8,22 @@ export function encryptCommand(program: Command) {
         .command("encrypt")
         .requiredOption("-i, --input <file>", "Input path")
         .requiredOption("-o, --output <file>", "Output path")
-        .requiredOption("-p, --password <password>", "Password for encryption")
-        .action((options) => {
+        // .requiredOption("-p, --password <password>", "Password for encryption")
+        .action(async (options) => {
             const inputPath = path.resolve(process.cwd(), options.input)
             const outputPath = path.resolve(process.cwd(), options.output)
-            encryptFile(inputPath, outputPath, options.password)
+
+            const answer = await inquirer.prompt([
+                {
+                    type: 'password',
+                    name: "password",
+                    message: 'Password:',
+                    mask: true
+                }
+            ])
+            // console.log(answer.password);
+            
+            encryptFile(inputPath, outputPath, answer.password)
         })
 }
 
@@ -20,10 +32,20 @@ export function decryptCommand(program: Command) {
         .command("decrypt")
         .requiredOption("-i, --input <file>", "Input path")
         .requiredOption("-o, --output <file>", "Output path")
-        .requiredOption("-p, --password <password>", "Password for decryption")
-        .action((options) => {
+        // .requiredOption("-p, --password <password>", "Password for decryption")
+        .action(async (options) => {
             const inputPath = path.resolve(process.cwd(), options.input)
             const outputPath = path.resolve(process.cwd(), options.output)
-            decryptFile(inputPath, outputPath, options.password)
+
+            const answer = await inquirer.prompt([
+                {
+                    type: 'password',
+                    name: "password",
+                    message: 'Password:',
+                    mask: true
+                }
+            ])
+
+            decryptFile(inputPath, outputPath, answer.password)
         })
 }
