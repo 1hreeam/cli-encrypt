8db1bc415bc89fbfa232708705b163e2342e0d4a
1hreeam <hypexund@gmail.com>
2025-12-30 22:55:48 +0100

    feat: clean up ora statements and add some trace logs

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,7 +1,5 @@
-import chalk from 'chalk';
 import crypto from 'crypto';
 import { readFileSync, writeFileSync } from 'fs';
-import ora from 'ora';
 import type { Logger } from './utils/logger.js';
 
 const ora_delay = 200;
@@ -10,11 +8,11 @@ export async function encryptFile(
   inputPath: string,
   outputPath: string,
   password: string,
-  logger: Logger,
+  logger: Logger
 ) {
   const inputBuffer = readFileSync(inputPath);
 
-  // const spinnerIV = ora('Generating initialization vector...').start();
+  logger.trace('Loaded the file into the buffer');
 
   // IV - Initialization Vector
   const iv = crypto.randomBytes(12);
@@ -22,52 +20,43 @@ export async function encryptFile(
 
   logger.info('Generated IV');
 
-  // await new Promise((r) => setTimeout(r, ora_delay));
-  // spinnerIV.succeed('Generated Initialization Vector');
-
-  // const spinnerEncrypting = ora('Encrypting data...').start();
-
   const key = crypto.scryptSync(password, salt, 32);
   const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
   const encrypted = Buffer.concat([cipher.update(inputBuffer), cipher.final()]);
   const authTag = cipher.getAuthTag();
 
+  logger.trace('Created key and authTag');
+
   const outputBuffer = Buffer.concat([iv, salt, authTag, encrypted]);
   writeFileSync(outputPath, outputBuffer);
 
   logger.info('Encrypted successfully');
-
-  // await new Promise((r) => setTimeout(r, ora_delay));
-  // spinnerEncrypting.succeed(chalk.greenBright('Encrypted sucessfully'));
 }
 
 export async function decryptFile(
   inputPath: string,
   outputPath: string,
   password: string,
-  logger: Logger,
+  logger: Logger
 ) {
   const inputBuffer = readFileSync(inputPath);
-
-  // const spinner1 = ora('Retrieving encryption data...');
 
   const iv = inputBuffer.subarray(0, 12);
   const salt = inputBuffer.subarray(12, 28);
   const authTag = inputBuffer.subarray(28, 44);
   const cipherText = inputBuffer.subarray(44);
 
-  // await new Promise((r) => setTimeout(r, ora_delay));
-  // spinner1.succeed('Retrieved encryption bytes');
-
-  // const spinnerDec = ora('Decrypting...').start();
+  logger.trace('Retieved secret data from the file');
 
   const key = crypto.scryptSync(password, salt, 32);
   const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv);
 
+  logger.trace('Generated key');
+
   try {
     decipher.setAuthTag(authTag);
   } catch (err) {
-    // spinnerDec.fail(chalk.redBright('Decryption failed'));
+    logger.error('Decryption failed');
     throw new Error('Decryption failed: wrong authTag');
   }
 
@@ -75,12 +64,10 @@ export async function decryptFile(
   try {
     decrypted = Buffer.concat([decipher.update(cipherText), decipher.final()]);
   } catch (err) {
-    // spinnerDec.fail(chalk.redBright('Decryption failed'));
+    logger.error('Decryption failed');
     throw new Error('Decryption failed: wrong password or corrupted file');
   }
 
   writeFileSync(outputPath, decrypted);
-  // spinnerDec.succeed(chalk.greenBright('Decrypted sucessfully'));
-
   logger.info('Decrypted successfully');
 }
